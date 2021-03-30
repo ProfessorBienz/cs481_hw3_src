dedc51b725becf49a7be7534ea397dc2a76822d9
Amanda Bienz <bienz@Amandas-Work-MacBook-Pro.local>
2021-03-30 17:50:48 -0600

    added queue init and destroy

--- queue.cpp
+++ queue.cpp
@@ -2,7 +2,7 @@
 
 void queue_init(queue_t* queue)
 {
-
+    queue* =  (queue_t*)malloc(sizeof(queue_t));
 }
 
 int queue_empty(queue_t* queue)
@@ -20,5 +20,10 @@ pthread_t queue_remove(queue_t* queue)
     pthread_t front_thread = queue->pthread_queue.front();
     queue->pthread_queue.pop();
     return front_thread;
+}
+
+pthread_t queue_destroy(queue_t* queue)
+{
+    free(queue);
 }
 
